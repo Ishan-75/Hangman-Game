40ca32841af710fb30ead7f73685aaa8a8d9937a
ishan <ishanrajavelu75@gmail.com>
2025-12-20 10:07:25 +0530

    js logic worked

---
+++ script.js
@@ -0,0 +1,136 @@
+"use scrict"
+//selecting elements
+let hangmanImg = document.getElementById(`hangman_image`)
+const hintText = document.getElementById(`hint`);
+const secretWord = document.getElementById(`secret_word`);
+const keys = document.querySelectorAll(`.keys`);
+let winOrLose = document.getElementById("won-or-lose-popup");
+let popup = document.getElementById("popup");
+const restart = document.querySelector(".restart");
+let wrongGuesses = 0
+
+
+//picking random word
+let random = words[Math.floor(Math.random() * words.length)];
+let selectedWord = random.word.toUpperCase();
+let hint = random.hint;
+hintText.innerText = (`Hint:${hint}`)
+
+
+//creating li tags
+secretWord.innerHTML = selectedWord.split("").map(() => `<li class="word-letter"></li>`).join("");
+
+
+// calculating letters to hide
+let minHide = Math.ceil((selectedWord.length) * 0.5);
+let maxHide = Math.ceil((selectedWord.length) * 0.7);
+let letterToHide = Math.floor(Math.random() * (maxHide - minHide + 1)) + minHide;
+
+
+//hiding ramdom letters in words
+let displayed = selectedWord.split("")
+let hideIndices = new Set();
+while (hideIndices.size < letterToHide) {
+     let randomIndex = Math.floor(Math.random() * selectedWord.length);
+     hideIndices.add(randomIndex)
+}
+hideIndices.forEach(i => {
+     secretWord.querySelectorAll("li")[i].innerText = displayed[i]
+})
+
+
+
+
+//function of keyboard
+keys.forEach(btn => {
+     let clickedLetter = btn.textContent.toUpperCase();
+     btn.addEventListener("click", function () {
+          if (selectedWord.includes(clickedLetter)) {
+               selectedWord.split("").forEach((letter, i) => {
+                    if (letter === clickedLetter) {
+                         secretWord.querySelectorAll("li")[i].innerText = letter;
+                         btn.classList.add("key-disabled")
+                         btn.disable = true;
+
+                         //popup
+                         let allLetterFound = [...secretWord.querySelectorAll("li")].every(li => li.textContent !== "");
+                         if (allLetterFound) {
+                              winOrLose.classList.add("won-or-lose-popup")
+                              popup.classList.add("popup")
+
+                              //congratsIMG
+                              let congratsImg = document.createElement("img");
+                              congratsImg.setAttribute("src", `media/icons/trophy.png`)
+                              congratsImg.classList.add("congratulation-img")
+                              popup.appendChild(congratsImg)
+
+                              //status
+                              let status = document.createElement("p");
+                              status.classList.add("status");
+                              status.innerText = `you won!!`;
+                              popup.appendChild(status);
+
+                              //info
+                              let info = document.createElement("p");
+                              info.classList.add("info");
+                              info.innerText = `wanna try again`;
+                              popup.appendChild(info);
+
+                              //restart button
+                              let restart = document.createElement("button");
+                              restart.classList.add("restart");
+                              restart.innerText = "restart";
+                              popup.appendChild(restart)
+                              restart.addEventListener("click", function () {
+                                   window.location.reload();
+                              })
+                         }
+                    }
+               })
+          }
+
+
+          else {
+
+               wrongGuesses++
+               hangmanImg.setAttribute("src", `media/hangman/lives-${wrongGuesses}.png`);
+               hangmanImg.classList.add("wrong-click");
+               setTimeout(() => {
+                    hangmanImg.classList.remove("wrong-click");
+               }, 100);
+
+               //popup
+               if (wrongGuesses >= 6) {
+                    winOrLose.classList.add("won-or-lose-popup");
+                    popup.classList.add("popup");
+                    //congratsIMG
+                    let congratsImg = document.createElement("img");
+                    congratsImg.setAttribute("src", `media/icons/lost.png`);
+                    congratsImg.classList.add("congratulation-img");
+                    popup.appendChild(congratsImg);
+                    //status
+                    let status = document.createElement("p");
+                    status.classList.add("status");
+                    status.innerText = `YOU LOST!!`;
+                    popup.appendChild(status);
+
+                    //info
+                    let info = document.createElement("p");
+                    info.classList.add("info");
+                    info.innerText = `wanna try again`;
+                    popup.appendChild(info);
+
+                    //restart button
+                    let restart = document.createElement("button");
+                    restart.classList.add("restart");
+                    restart.innerText = "restart";
+                    popup.appendChild(restart);
+                    restart.addEventListener("click", function () {
+                         window.location.reload();
+                    })
+               }
+          }
+     })
+})
+
+console.log(displayed)
---
+++ words-data.js
@@ -0,0 +1,68 @@
+const words = [
+
+    //animal
+
+    { word: "Chimpanzee", hint: "Known for using tools and deep jungle chatter." },
+    { word: "Alligator", hint: " Ancient reptile with powerful jaws and a grin-like snout.." },
+    { word: "Kangaroos", hint: "Masters of hopping and carrying their young in pouches." },
+    { word: "Crocodile", hint: "Patient river stalker with prehistoric armor." },
+    { word: "Hippopotamus", hint: "Big, semi - aquatic grazer with a short temper." },
+    { word: "Porcupine", hint: "Defensive creature covered with sharp quills." },
+    { word: "Armadillo", hint: "Armored digger that rolls into a ball." },
+    { word: "Chameleon", hint: "Expert in blending colors with its surroundings." },
+    { word: "Rhinoceros", hint: "Massive grazer with a prominent horn on its nose." },
+    { word: "Dragonfly", hint: "Aerial acrobat found near ponds with shimmering wings." },
+
+
+    //fruits
+    { word: "Pineapple", hint: "Tropical fruit with a spiky crown and golden sweetness." },
+    { word: "Blackberry", hint: "Dark, juicy berry often found in thorny bushes." },
+    { word: "Pomegranate", hint: "Red sphere filled with ruby-like seeds." },
+    { word: "Watermelon", hint: "Picnic favorite—green shell, red heart." },
+    { word: "Raspberry", hint: "Tiny cluster fruit with a soft tartness." },
+    { word: "Strawberry", hint: "Heart-shaped fruit with dotted surface." },
+    { word: "Mangosteen", hint: "Sweet purple “queen” of tropical fruits." },
+    { word: "Cranberry", hint: "Tart berry often enjoyed in juice or sauce." },
+    { word: "Passionfruit", hint: "Wrinkled shell hiding aromatic pulp." },
+    { word: "Blueorange", hint: "A mythical blend of color and zest (trick fruit!)." },
+
+
+    //foods
+    { word: "Spaghetti", hint: "Long strands often twirled in tomato sauce." },
+    { word: "Croissant", hint: "Flaky pastry that began in France." },
+    { word: "Lasagna", hint: "Layered pasta dish baked with cheese and sauce." },
+    { word: "Cheeseburg", hint: "Savory sandwich loved worldwide for its melty center." },
+    { word: "Chocolate ", hint: "Sweet treat born from cacao beans." },
+    { word: "Casserole", hint: "Oven-baked mix of comfort and carbs." },
+    { word: "Pancake", hint: "Flat breakfast favorite served with syrup." },
+    { word: "Sandwiche", hint: "Edible invention with endless filling varieties." },
+    { word: "Cappuccino", hint: "Frothy coffee with a creamy foam top." },
+    { word: "Doughnut", hint: "Round, ringed, and sugar-dusted indulgence." },
+
+    //sports
+    { word: "Basketball", hint: "Game of hoops, dribbles, and slam dunks." },
+    { word: "Badminton", hint: "Racket sport played with a feathered projectile." },
+    { word: "Volleyball", hint: "Beach or court action with high-flying spikes." },
+    { word: "Snowboarding", hint: "Gliding gracefully down wintry slopes." },
+    { word: "Tabletennis", hint: "Miniature version of a paddle-based rivalry." },
+    { word: "Windsurfing", hint: "Combines wave riding with wind mastery." },
+    { word: "Gymnastics", hint: "Artistic flips and gravity-defying strength." },
+    { word: "Baseballer ", hint: "Bat, cap, glove, and bases define it." },
+    { word: "Cricketing", hint: "Gentlemans game played with bat and wickets." },
+    { word: "Bodyweight", hint: "Fitness regime relying purely on self-resistance." },
+
+
+    //vegetables
+    { word: "Cauliflower", hint: "White florets often disguised as rice or mash." },
+    { word: "Broccoli", hint: "Tree-like green packed with nutrients." },
+    { word: "Asparagus", hint: "Tender stalks often served grilled or steamed." },
+    { word: "Artichoke ", hint: "Layered veggie with an edible heart." },
+    { word: "Zucchinii", hint: "Green squash perfect for stir-fries." },
+    { word: "Cucumber", hint: "Cool and crisp salad favorite." },
+    { word: "Spinachie", hint: "Leafy green Popeye adored." },
+    { word: "Eggplants", hint: "Glossy purple veggie with spongy flesh." },
+    { word: "Kohlrabis", hint: "Bulbous stem vegetable from the cabbage clan." },
+    { word: "Coriander", hint: "Herb with fragrant leaves and versatile seeds." },
+
+]
+window.words = words 
